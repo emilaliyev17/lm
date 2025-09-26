625c1484d0ff8dbc901772cd68917c1ae9ccc78a
Emil Aliyev <emil.aliyev@icloud.com>
2025-09-26 16:41:34 -0700

    Temporarily disable migrations for initial deploy

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+# Миграции временно отключены - запустим их вручную после деплоя
+# python manage.py migrate --no-input
